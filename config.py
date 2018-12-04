e7ef77ea5bbc25aad69eeb390df07037170b4391
jvazquez96 <jorgevzqz6@gmail.com>
2018-12-04 17:56:20 -0600

    c

--- config.py
+++ config.py
@@ -8,12 +8,14 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
+
     SQLALCHEMY_DATABASE_URI = 'postgresql:///t247_dev'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     # os.environ.get("DATABASE_URL")
     # postgres://sbmvjnstodirdq:da2890133f95e99bfcf7cb5a3631e9b164ce2e8cd570e235aa718ba47a83230b@ec2-174-129-32-37.compute-1.amazonaws.com:5432/demqd7v1u6naso
     SQLALCHEMY_TRACK_MODIFICATIONS = True
